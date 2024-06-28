2fbf662f243c448282a8d4ab3df7c3ae9de65ee6
Saccharin01 <chousik01@gmail.com>
2024-06-28 15:04:02 +0900

    feat | mimeType.js
    mimeType 모듈 의 key 값을 조정합니다

--- module/mimeTypes.js
+++ module/mimeTypes.js
@@ -1,15 +1,14 @@
 const mimeTypes = {
-  'html': 'text/html',
-  'css': 'text/css',
-  'js': 'application/javascript',
-  'png': 'image/png',
-  'jpg': 'image/jpeg',
-  'jpeg': 'image/jpeg',
-  'gif': 'image/gif',
-  'svg': 'image/svg+xml',
-  'json': 'application/json',
-  'txt': 'text/plain',
+  html: "text/html",
+  css: "text/css",
+  js: "application/javascript",
+  png: "image/png",
+  jpg: "image/jpeg",
+  jpeg: "image/jpeg",
+  gif: "image/gif",
+  svg: "image/svg+xml",
+  json: "application/json",
+  txt: "text/plain",
 };
-
 
 module.exports = mimeTypes;
