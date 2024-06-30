df0caaf9fceea514f9e120edc818ef2da74bdbd2
Saccharin01 <chousik01@gmail.com>
2024-07-01 04:18:00 +0900

    new | readFileModule.js
    파일을 읽어들이는 모듈을 제작했습니다. 해당 모듈은 지정된 경로의 정적 파일을 동적으로 읽어들여 사용합니다.

---
+++ module/readFileModule.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const mimeType = require('./mimeTypes.js')
+const path = require('path')
+const errorMsg = require('./errorMsg.js')
+
+/**
+ * 파일을 동적으로 처리해주기 위한 함수입니다
+ * 
+ * @param res 
+ * @param arguPath readFile에 매개변수로 들어가는 경로입니다.
+ */
+
+const readFileModule = (arguPath,res)=>{
+
+  let mimeIndex = path.extname(arguPath).substring(1)
+
+  fs.readFile(arguPath,(err,data)=>{
+    try {
+
+    res.writeHead(200,{"content-Type": mimeType[mimeIndex]})
+    res.end(data)
+
+    } catch (error) {
+      errorMsg(error,res)
+    }
+  })
+}
+
+module.exports = readFileModule
+// readFileModule("/module/reDirect.js","")
