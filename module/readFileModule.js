f121c596413418709c66c5baed2811bd9e88c9a3
Saccharin01 <chousik01@gmail.com>
2024-07-01 12:12:15 +0900

    feat | readFileModule.js
    path모듈의 의존성을 제거하고 mimeIndex를 매개변수로 받게끔 함수를 수정했습니다

--- module/readFileModule.js
+++ module/readFileModule.js
@@ -1,19 +1,16 @@
 const fs = require('fs')
 const mimeType = require('./mimeTypes.js')
-const path = require('path')
 const errorMsg = require('./errorMsg.js')
 
 /**
  * 파일을 동적으로 처리해주기 위한 함수입니다
  * 
  * @param res 
- * @param arguPath readFile에 매개변수로 들어가는 경로입니다.
+ * @param arguPath readFile에 매개변수asda로 들어가는 경로입니다.
+ * @param mimeIndex mimeType 모듈에서 동적으로 contentType을 적용하기 위한 매개변수 입니다
  */
 
-const readFileModule = (arguPath,res)=>{
-
-  let mimeIndex = path.extname(arguPath).substring(1)
-
+const readFileModule = (arguPath,mimeIndex,res)=>{
   fs.readFile(arguPath,(err,data)=>{
     if(err){
       console.error(`readFile Error : ${err}`)
