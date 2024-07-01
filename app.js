ec4000b5dd1b239b6e04f1b7dc3a575b6c9833f8
Saccharin01 <chousik01@gmail.com>
2024-07-01 09:34:17 +0900

    feat | app.js
    주석 처리되어있던 중복 코드 블럭을 제거합니다. 또한 테스트 과정에서 작성되었던 중복 코드도 같이 정리했습니다.

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ const fileServe = require('./module/fileServe')
 
 
 let server = http.createServer((req, res)=>{    
-  if(req.url.startsWith("/module")){
-    const normalize = path.normalize(req.url)
-    let test = path.extname(normalize).substring(1)
-    const filePath = path.join(__dirname,normalize)
-    console.log(filePath)
-    console.log(test)
 
-  }
   console.log(req.method,"  and  ",req.url)
 
   if(req.method === "GET"){
@@ -33,86 +26,10 @@ let server = http.createServer((req, res)=>{
         }
       })
     } else {
+
       fileServe(req,res,__dirname)
 
     }
-
-
-
-    // else if(req.url.split(".")[1]=== "html"){
-    //    // console.log(__dirname)
-    //   let landPath = path.join(__dirname,"public","html",req.url.split("/")[1])
-    //   // console.log(htmlpath)
-    //   fs.readFile(landPath,"utf-8",(err,data)=>{
-    //     if(err){
-    //       res.writeHead(500, {"content-type" : "text/plain"})
-    //       res.write("server error")
-    //     } else {
-    //       // console.log(data)
-    //       res.writeHead(200, {"content-type" : "text/html"})
-    //       res.write(data)
-    //       res.end()
-    //     }
-    //   })
-    // }
-    // else if(req.url.split(".")[1] === "png"){
-    //   let imagePath = path.join(__dirname,"public","resource",req.url.split("/")[1])
-    //   fs.readFile(imagePath,(err,data)=>{
-    //     if(err){
-    //       res.writeHead(500, {"content-type" : "text/plain"})
-    //       res.write("server error")
-    //     } else {
-    //       res.writeHead(200, {"content-type" : "image/png"})
-    //       res.write(data)
-    //       res.end()
-    //     }
-    //   })
-    // }
-    // else if(req.url.split(".")[1] === "js"){
-    //   // console.log(__dirname)
-    //   let jspath = path.join(__dirname,"public",req.url)
-    //   // console.log(jspath)
-    //   fs.readFile(jspath,"utf-8",(err,data)=>{
-    //     if(err){
-    //       res.writeHead(500, {"content-type" : "text/plain"})
-    //       res.write("server error")
-    //     } else {
-    //       // console.log(data)
-    //       res.writeHead(200, {"content-type" : "application/javaScript"})
-    //       res.write(data)
-    //       res.end()
-    //     }
-    //   })
-    // }
-    // else if(req.url.split(".")[1] === "css"){
-    //   let csspath = path.join(__dirname,"public","css",req.url.split("/")[1])
-    //   // console.log(jspath)
-    //   fs.readFile(csspath,"utf-8",(err,data)=>{
-    //     if(err){
-    //       res.writeHead(500, {"content-type" : "text/plain"})
-    //       res.write("server error")
-    //     } else {
-    //       // console.log(data)
-    //       res.writeHead(200, {"content-type" : "text/css"})
-    //       res.write(data)
-    //       res.end()
-    //     }
-    //   })
-    // }
-    // else if(req.url.startsWith("/list")){
-    //   let landPath = path.join(__dirname,"public","html","list.html")
-    //   fs.readFile(landPath,(err,data)=>{
-    //     if(err){
-    //       console.log(err)
-    //       res.end("오류")
-    //     }else{
-    //       res.write(data)
-    //       res.end()
-          
-    //     }
-    //   })
-
-    // }
   }
 })
 
