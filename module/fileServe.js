9f717643835227d4cbd77e1bd63ef1004b21a708
Saccharin01 <chousik01@gmail.com>
2024-07-01 09:29:23 +0900

    feat | fileServe.js
    fileServe 모듈을 설명하는 주석을 추가하고, 불필요한 내장 모듈 호출을 제거합니다.

--- module/fileServe.js
+++ module/fileServe.js
@@ -1,13 +1,21 @@
-const fs = require('fs')
 const path = require('path')
 const readFileModule = require('./readFileModule')
 
+/**
+ * 
+ * readFileModule을 통해서 읽어온 파일에 대한 경로 처리를 도와주는 모듈입니다.
+ * 
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} rootDir 코드가 실행되는 파일을 기준으로 계산될 기초 경로입니다.
+ */
 
-const fileRead = (req,res,rootDir)=>{
+const fileServe = (req,res,rootDir)=>{
   const decodedUrl = decodeURIComponent(req.url);
   const normalize = path.normalize(decodedUrl)
-  console.log(normalize)
-  console.log(decodedUrl)
+  // console.log(normalize)
+  // console.log(decodedUrl)
 
   let filePath;
   if(req.url.startsWith("/module")){
@@ -22,6 +30,6 @@ const fileRead = (req,res,rootDir)=>{
 
 
 
-module.exports = fileRead
+module.exports = fileServe
 // let req = "/module/mimeType.js"
 // fileRead(req,"")
