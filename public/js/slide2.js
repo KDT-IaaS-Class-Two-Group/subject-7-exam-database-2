4b4b999592805c69ed601062fa23d6197c72c320
Saccharin01 <chousik01@gmail.com>
2024-07-02 15:57:28 +0900

    fix slide2.html / slide2.js
    병합 충돌로 발생한 부분의 순서 처리를 옮겼습니다.
    html 내에 중복 작성된 script 태그를 정리했습니다.

--- public/js/slide2.js
+++ public/js/slide2.js
@@ -1,6 +1,5 @@
 import {ListRequest} from "../../module/ListRequest.js"
 import { modalControl } from "../../module/modalControl.js";
 
-const itemListData = await ListRequest();
-
 modalControl()
+const itemListData = await ListRequest();
