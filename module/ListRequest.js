41b60efba4fb3eab400122ad9ecc60dd6a806718
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-02 13:09:14 +0900

    Fix | ListRequest.js : fetch url 수정

--- module/ListRequest.js
+++ module/ListRequest.js
@@ -1,6 +1,6 @@
 export const ListRequest = async () => {
 
-  const response = await fetch(`${window.location.host}/list`);
+  const response = await fetch(`http://${window.location.host}/list`);
   const data = await response.json();
 
   return data;
