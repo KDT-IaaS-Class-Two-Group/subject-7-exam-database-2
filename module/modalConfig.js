df72826b2ee15776669b54f691cfc9682da3b7cd
Saccharin01 <chousik01@gmail.com>
2024-07-05 09:59:30 +0900

    new | modalConfig.js
    모달 창의 클래스 와 id를 저장한 객체를 분리

---
+++ module/modalConfig.js
@@ -0,0 +1,14 @@
+const modalConfig = {
+  id: 'modal',
+  class: [
+    'modal-content',
+    'close-button',
+    'modal-header',
+    'modal-image',
+    'product-image',
+    'modal-body',
+    'modal-section',
+    'buy-sell'
+  ],
+  contentParameter : 3
+}
