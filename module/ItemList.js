40a74dbf73e702ccebd89c19cb775c9c5c94a9d3
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-05 09:52:12 +0900

    Fix | ItemList.js : 프로퍼티 src에 파일 경로를 붙여 반환하도록 함

--- module/ItemList.js
+++ module/ItemList.js
@@ -1,11 +1,19 @@
 const db = require("./CustomDB");
 const mimeType = require("./mimeTypes");
+require('dotenv').config();
 
 const ItemList = async (res) => {
   try{
     const result = db.all("select * from items");
+
+    for(let obj of result){
+      if('src' in obj){
+        obj["src"] = process.env.IMG_PATH + obj["src"] + process.env.IMG_EXTENSION;
+      }
+    }
+
     const data = JSON.stringify(result);
-    res.writeHead(200,{"content-Type": '${mimeType.json}'})
+    res.writeHead(200,{"content-Type": `${mimeType.json}`})
     res.end(data)
   } catch(err){
     res.statusCode = 400;
