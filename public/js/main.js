713779d33e0920b86b13623928510349f4750491
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-09 10:08:36 +0900

    Feat | main.js : AdjustScrollConverter Import

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 import {ListRequest} from "../../module/ListRequest.js"
+import { AdjustScrollConverter } from "./Controllers/AdjustScrollConverter.js";
 import { AddObjIntoItemList } from "./modules/AddItemList.js";
 
 const itemListData = await ListRequest();
 AddObjIntoItemList(itemListData);
+AdjustScrollConverter();
