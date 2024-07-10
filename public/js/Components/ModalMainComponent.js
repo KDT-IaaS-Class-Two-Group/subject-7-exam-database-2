96eb2b773358ba6b20d16917e0da13b4f77a676e
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-10 21:18:20 +0900

    Fix | ModalMainComponent.js : 매개변수로 style을 넣을 수 있게 수정

--- public/js/Components/ModalMainComponent.js
+++ public/js/Components/ModalMainComponent.js
@@ -1,9 +1,10 @@
-export const ModalMainComponent = (name, color, ...component) => {
+export const ModalMainComponent = (name, style, ...component) => {
   component = component !== undefined ? component.join("") : "";
+  style = style !== undefined ? style : "";
 
   return `
     <div class="modal-main">
-      <div class="modal-main-header">${name}</div>
+      <div class="modal-main-header" style="${style}">${name}</div>
       ${component}
     </div>
       `
